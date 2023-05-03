e9295894ffbcc6a630e9f73d10cede3d2cd07482
Rahul Nair <rahulunair@users.noreply.github.com>
2023-05-03 11:19:05 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,26 +12,78 @@ def mkdirs(name: str):
 
 def plot_latency_results(configurations, latencies):
     data = {
-        "Configuration": configurations,
-        "Latency": latencies,
+        "Configuration": [],
+        "Average Latency": [],
+        "Mean Latency": [],
+        "Median Latency": [],
+        "Standard Deviation": [],
+        "90th Percentile Latency": [],
+        "99th Percentile Latency": [],
     }
+    for config, latency in zip(configurations, latencies):
+        data["Configuration"].append(config)
+        data["Average Latency"].append(latency["average_latency"])
+        data["Mean Latency"].append(latency["mean_latency"])
+        data["Median Latency"].append(latency["median_latency"])
+        data["Standard Deviation"].append(latency["stdev_latency"])
+        data["90th Percentile Latency"].append(latency["90th_percentile_latency"])
+        data["99th Percentile Latency"].append(latency["99th_percentile_latency"])
     df = pd.DataFrame(data)
     with plt.xkcd():
-        plt.figure(figsize=(12, 6))
-        sns.barplot(x="Configuration", y="Latency", data=df)
-        plt.title("Stable diffusion latency results.")
-        plt.xlabel("Configuration")
-        plt.ylabel("Latency (seconds)")
-        plt.xticks(rotation=45)
-        plt.savefig("stable_diffussion_latency_results.png", bbox_inches="tight")
-        plt.show()
+        fig, axes = plt.subplots(3, 2, figsize=(15, 12))
+        fig.suptitle("Stable diffusion latency results")
+        sns.barplot(ax=axes[0, 0], x="Configuration", y="Mean Latency", data=df)
+        axes[0, 0].set_title("Mean Latency")
+        axes[0, 0].set_ylabel("Latency (seconds)")
+        axes[0, 0].set_xticklabels(labels=df["Configuration"], rotation=45)
+        
+        sns.barplot(ax=axes[0, 1], x="Configuration", y="Median Latency", data=df)
+        axes[0, 1].set_title("Median Latency")
+        axes[0, 1].set_ylabel("Latency (seconds)")
+        axes[0, 1].set_xticklabels(labels=df["Configuration"], rotation=45)
+
+        sns.barplot(ax=axes[1, 0], x="Configuration", y="Standard Deviation", data=df)
+        axes[1, 0].set_title("Standard Deviation")
+        axes[1, 0].set_ylabel("Latency (seconds)")
+        axes[1, 0].set_xticklabels(labels=df["Configuration"], rotation=45)
+
+        sns.barplot(ax=axes[1, 1], x="Configuration", y="90th Percentile Latency", data=df)
+        axes[1, 1].set_title("90th Percentile Latency")
+        axes[1, 1].set_ylabel("Latency (seconds)")
+        axes[1, 1].set_xticklabels(labels=df["Configuration"], rotation=45)
+
+        sns.barplot(ax=axes[2, 0], x="Configuration", y="99th Percentile Latency", data=df)
+        axes[2, 0].set_title("99th Percentile Latency")
+        axes[2, 0].set_ylabel("Latency (seconds)")
+        axes[2, 0].set_xticklabels(labels=df["Configuration"], rotation=45)
+        
+        sns.boxplot(ax=axes[2, 1], x="Configuration", y="Average Latency", data=df)
+        axes[2, 1].set_title("Average Latency Distribution")
+        axes[2, 1].set_ylabel("Latency (seconds)")
+        axes[2, 1].set_xticklabels(labels=df["Configuration"], rotation=45)
+
+        plt.tight_layout()
+        plt.subplots_adjust(top=0.9)
+        plt.savefig("sd_latency_plot.png", bbox_inches="tight")
 
 
 def save_results_to_csv(configurations, latencies, filename="sd_latency.csv"):
     results_path = mkdirs("results")
-    with open(os.path.join(results_path,filename), mode="w", newline="") as csvfile:
-        fieldnames = ["config", "latency"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for config, latency in zip(configurations, latencies):
-            writer.writerow({"config": config, "latency": latency})
+    data = {
+        "Configuration": configurations,
+        "Average Latency": [],
+        "Mean Latency": [],
+        "Median Latency": [],
+        "Standard Deviation": [],
+        "90th Percentile Latency": [],
+        "99th Percentile Latency": [],
+    }
+    for latency in latencies:
+        data["Average Latency"].append(latency["average_latency"])
+        data["Mean Latency"].append(latency["mean_latency"])
+        data["Median Latency"].append(latency["median_latency"])
+        data["Standard Deviation"].append(latency["stdev_latency"])
+        data["90th Percentile Latency"].append(latency["90th_percentile_latency"])
+        data["99th Percentile Latency"].append(latency["99th_percentile_latency"])
+    df = pd.DataFrame(data)
+    df.to_csv(os.path.join(results_path, filename), index=False)
