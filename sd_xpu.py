a0ea8863fa1bf79514cfa837ec12024315ff6779
Rahul Nair <rahulunair@users.noreply.github.com>
2023-05-03 11:06:26 -0700

    Update sd_xpu.py

--- sd_xpu.py
+++ sd_xpu.py
@@ -101,10 +101,10 @@ def run_experiment(
     percentile_99 = np.percentile(loop_time, 99)
 
     return {
-        'average_latency': latency,
-        'mean_latency': mean_latency,
-        'median_latency': median_latency,
-        'stdev_latency': stdev_latency,
-        '90th_percentile_latency': percentile_90,
-        '99th_percentile_latency': percentile_99
+        "average_latency" : latency,
+        "mean_latency" : mean_latency,
+        "median_latency" : median_latency,
+        "stdev_latency" : stdev_latency,
+        "90th_percentile_latency" : percentile_90,
+        "99th_percentile_latency" : percentile_99
     }
